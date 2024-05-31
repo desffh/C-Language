8adecf99c66b53b2d0ee9e0176021d0ba91499d2
desffh <yangju058@gmail.com>
2024-05-31 19:47:44 +0900

    (2024.5.31) 복사 생략

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,25 +1,113 @@
 ﻿#include <iostream>
 
+
 using namespace std;
+
+class Item
+{
+private:
+	int price;
+
+public:
+	Item(int price)
+	{
+		cout << "Constructor" << endl;
+		this->price = price;
+	}
+	Item(Item& item)
+	{
+		cout << "Copy Constructor" << endl;
+
+		price = item.price;
+	}
+};
+
+class Card
+{
+public:
+	virtual void Show() = 0;
+	virtual void Skill() = 0; // 순수 가상함수
+	virtual void Effect() = 0;
+};
+
+class Legend :Card
+{
+public:
+	void Show() override
+	{
+		cout << "Legend Card" << endl;
+	}
+	void Skill() override
+	{
+		cout << "Legend Skill" << endl;
+	}
+	void Effect() final
+	{
+		cout << "Legend Effect" << endl;
+	}
+};
+
+class Unique :Legend
+{
+public:
+	Unique()
+	{
+
+	}
+
+	//void Effect() override // 재정의 할 수 없음
+	//{
+	//	cout << "Unique Effect" << endl;
+	//}
+};
+
 
 int main()
 {
-#pragma region 연관 컨테이너
-	// 컨테이너 내의 잘들이 규칙에 따라 
-	// 정렬되어 있는 컨테이너입니다.
-	
-	//std::set<int> set;
+#pragma region R Value & L Value
+	// L Value
+	/*
+	//// L value Type
+	//int data = 10;
 	//
-	//std::insert(50);
-	//std::insert(10);
-	//std::insert(50);
-	//std::insert(50);
-	//std::insert(50);
+	//int& left1 = data;
+	//int& left1 = 10; //안됨
+	*/
+	// R Value
+	/*
+	// R value Type
+	int count = 0;
 
-	// set의 경우 KEY값이 중복이 되면 값이 그대로 들어옵니다.
+	int&& right1 = 10;
+	//int&& right2 = count; // 안됨 
+
+	right1 = 30;
+
+	cout << "right1의 값: " << right1 << endl;
+	*/
+
+
 
 #pragma endregion
 
+#pragma region 복사 생략(Copy Elision)
+	// 함수의 반환 값을 모두 사용하거나 초기화하는 경우에
+	// 생기는 불필요한 임시 객체를 최적화하거나 제거하는데
+	// 사용되는 컴파일러 기술입니다.
+
+	// Item item1(10000);
+	// Item item2(item1);
+
+
+
+
+
+#pragma endregion
+
+#pragma region final
+
+
+#pragma endregion
 
 
 	return 0;
